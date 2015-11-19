f0b81e1aa2e7b5c34fd58c20e8d56874a7d6f500
Andrey Kvichanskiy <kvichans@mail.ru>
2015-11-19 17:42:52 +0300

    fix "opt corruption"

--- __init__.py
+++ __init__.py
@@ -6,7 +6,7 @@ Version:
 '''
 #! /usr/bin/env python3
 
-import  re, json, os, webbrowser, tempfile, collections, html
+import  re, json, os, webbrowser, tempfile, collections, html, pickle
 import  cudatext        as app
 from    cudatext    import ed
 import  cudax_lib       as apx
@@ -61,10 +61,20 @@ def do_report(fn):
     usr_json    = os.path.join(app.app_path(app.APP_DIR_SETTINGS), 'user.json')
     lex_json    = os.path.join(app.app_path(app.APP_DIR_SETTINGS), 'lexer {}.json'.format(lex))
     def_opts    = apx.get_app_default_opts()
+#   pass;                       LOG and log('?? get_app_default_opts')
 #   def_opts    = apx.get_app_default_opts(         object_pairs_hook=collections.OrderedDict)
+    
+    if 'font_name' != list(def_opts.keys())[0]:
+        pass;                   apx.log('Not natural order')
+        return False
+    
     usr_opts    = apx._get_file_opts(usr_json, {},  object_pairs_hook=collections.OrderedDict)
-#   pass;                      apx.log('usr_opts=\n{}', usr_opts)
     lex_opts    = apx._get_file_opts(lex_json, {},  object_pairs_hook=collections.OrderedDict)
+
+    def_opts    = pickle.loads(pickle.dumps(def_opts))  # clone to pop
+    usr_opts    = pickle.loads(pickle.dumps(usr_opts))  # clone to pop
+    lex_opts    = pickle.loads(pickle.dumps(lex_opts))  # clone to pop
+
     fil_opts    = get_ovrd_ed_opts(ed)
     cmt_opts    = {}
     # Find Commentary for def opts in def file
@@ -73,18 +83,13 @@ def do_report(fn):
     def_body    = def_body.replace('\r\n', '\n').replace('\r', '\n')
     def_body    = def_body[def_body.find('{')+1:]   # Cut head with start '{'
     def_body    = def_body.lstrip()
-    pass;                      #LOG and apx.log('pr for def_body={}',def_body[:50])
     for opt in def_opts.keys():
         pos_opt = def_body.find('"{}"'.format(opt))
-        pass;                  #LOG and apx.log('opt, pos_opt={}',(opt, pos_opt))
         cmt     = def_body[:pos_opt].strip()
         cmt     = re.sub('^\s*//', '', cmt, flags=re.M)
         cmt     = cmt.strip()
-        pass;                  #LOG and apx.log('cmt={}',cmt)
         cmt_opts[opt]    = html.escape(cmt)
         def_body= def_body[def_body.find('\n', pos_opt)+1:]   # Cut the opt
-        pass;                  #LOG and apx.log('pr for def_body={}',def_body[:50])
-       #pass;                   return False
 
     with open(fn, 'w', encoding='utf8') as f:
         f.write(RPT_HEAD)
